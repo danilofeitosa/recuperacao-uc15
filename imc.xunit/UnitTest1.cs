6661e6756937accbf58110cc0bb48f155efe99fe
Danilo <danilo.feitosa94@hotmail.com>
2024-09-09 17:11:45 -0300

    recuperacao-uc-15

---
+++ imc.xunit/UnitTest1.cs
@@ -0,0 +1,36 @@
+namespace imc.xunit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Teste_Calculo_IMC()
+    {
+        double imc_previsto = 31.25;
+        IMC i = new IMC();
+        i.peso = 80;
+        i.altura = 1.60;
+
+        i.Calcular_IMC();
+
+        Assert.Equal(i.imc, imc_previsto);
+    }
+    [Theory]
+    [InlineData("Abaixo do peso")]
+    [InlineData("Peso normal")]
+    [InlineData("Sobrepeso")]
+    [InlineData("Obesidade Grau I")]
+    [InlineData("Obesidade Grau II")]
+    [InlineData("Obesidade Grau III")]
+
+    public void Teste_Categoria_IMC(String cat) {
+        IMC i = new IMC();
+        i.peso = 80;
+        i.altura = 1.60;
+
+        i.Calcular_IMC();
+        i.Classificar_IMC();
+
+        Assert.Equal(i.categoria, cat);
+    }
+
+}
